0222bf45b39b9f10b3c01b134c3c2a5f237c1ebd
Engin <denganjun1234@qq.com>
2016-03-26 21:26:51 +0800

    less ; watch

--- src/gulp-demo-1/gulpfile.js
+++ src/gulp-demo-1/gulpfile.js
@@ -1,10 +1,25 @@
-var gulp = require('gulp')
-var connect = require('gulp-connect')
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    watch = require('gulp-watch')
 
 
 gulp.task('http-server', function() {
-    connect.server()
+    connect.server({
+        livereload: true,
+        root: ['src']
+    })
 })
 
 
-gulp.task('default', ['http-server'])
+gulp.task('watch', function() {
+    gulp.watch("style/*.less", ['less'])
+})
+
+gulp.task('livereload', function() {
+    gulp.src(['src/style/*less', 'src/*html', 'src/script/*js'])
+        .pipe(watch())
+        .pipe(connect.reload())
+})
+
+
+gulp.task('default', ['http-server', 'watch', 'livereload'])
