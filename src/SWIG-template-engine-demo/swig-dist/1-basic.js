fbca5003ed28b0f860564176f5c9b117c58e30ba
Engin <denganjun1234@qq.com>
2016-04-07 11:25:36 +0800

    swig in browser , it not work

--- src/SWIG-template-engine-demo/swig-dist/1-basic.js
+++ src/SWIG-template-engine-demo/swig-dist/1-basic.js
@@ -8,7 +8,7 @@ _output += "</h1>\n	<ul>\n	";
 (function () {
   var __l = (((typeof _ctx.authors !== "undefined" && _ctx.authors !== null) ? ((typeof _ctx.authors !== "undefined" && _ctx.authors !== null) ? _ctx.authors : "") : ((typeof authors !== "undefined" && authors !== null) ? authors : "")) !== null ? ((typeof _ctx.authors !== "undefined" && _ctx.authors !== null) ? ((typeof _ctx.authors !== "undefined" && _ctx.authors !== null) ? _ctx.authors : "") : ((typeof authors !== "undefined" && authors !== null) ? authors : "")) : "" ), __len = (_utils.isArray(__l) || typeof __l === "string") ? __l.length : _utils.keys(__l).length;
   if (!__l) { return; }
-    var _ctx__loopcache07264097856823355 = { loop: _ctx.loop, author: _ctx.author, __k: _ctx.__k };
+    var _ctx__loopcache037687067524529994 = { loop: _ctx.loop, author: _ctx.author, __k: _ctx.__k };
     _ctx.loop = { first: false, index: 1, index0: 0, revindex: __len, revindex0: __len - 1, length: __len, last: false };
   _utils.each(__l, function (author, __k) {
     _ctx.author = author;
@@ -25,10 +25,10 @@ _output += _filters["e"]((((typeof _ctx.author !== "undefined" && _ctx.author !=
 _output += "</li>\n	";
     _ctx.loop.index += 1; _ctx.loop.index0 += 1; _ctx.loop.revindex -= 1; _ctx.loop.revindex0 -= 1;
   });
-  _ctx.loop = _ctx__loopcache07264097856823355.loop;
-  _ctx.author = _ctx__loopcache07264097856823355.author;
-  _ctx.__k = _ctx__loopcache07264097856823355.__k;
-  _ctx__loopcache07264097856823355 = undefined;
+  _ctx.loop = _ctx__loopcache037687067524529994.loop;
+  _ctx.author = _ctx__loopcache037687067524529994.author;
+  _ctx.__k = _ctx__loopcache037687067524529994.__k;
+  _ctx__loopcache037687067524529994 = undefined;
 })();
 _output += "\n	</ul>\n	\n	\n</body>\n</html>\n";
 
