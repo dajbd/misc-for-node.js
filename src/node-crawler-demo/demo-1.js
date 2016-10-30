754d411a016ac82c8e7ef7fc618f6ad0b277b826
Engin <denganjun1234@qq.com>
2016-10-31 00:16:10 +0800

    node crawler/spider demo 1

---
+++ src/node-crawler-demo/demo-1.js
@@ -0,0 +1,25 @@
+var Crawler = require("crawler");
+var url = require('url');
+
+
+var c = new Crawler({
+    maxConnections: 10,
+    jQuery: 'cheerio',
+    // This will be called for each crawled page
+    callback: function(error, result, $) {
+        // console.log(result)
+        // console.log($, typeof result, '\n--------', ++count)
+        // $ is Cheerio by default
+        //a lean implementation of core jQuery designed specifically for the server
+        // $('a').each(function(index, a) {
+        //     var toQueueUrl = $(a).attr('href');
+        //     c.queue(toQueueUrl);
+        // });
+
+        // $ && console.log($('title').text())
+        console.log($('title').text())
+    }
+});
+
+// Queue a list of URLs
+c.queue(['http://baike.so.com/doc/2707954-2858984.html']);
