13ddabfe21ed38fe1425780d90d0a7595af11e89
Engin <denganjun1234@qq.com>
2016-11-22 22:04:34 +0800

    comment

--- src/pinyin-hotoo-Github-demo/demo-1.js
+++ src/pinyin-hotoo-Github-demo/demo-1.js
@@ -7,6 +7,11 @@ console.log(pinyin("中心", {
 console.log(pinyin("中心", {
   heteronym: true,              // 启用多音字模式
   // segment: true                 // 启用分词，以解决多音字问题。
+  // 多音字、分词等功能，依赖的模块很多，在windows安装那些东西有些麻烦
+  // 暂时不启用此功能
+  // 参考 https://cnodejs.org/topic/510a98acdf9e9fcc58ee157b
+  // 文中说到的依赖：.net framework 4.5.1, python 2.7 等
+
 }));                            // [ [ 'zhōng' ], [ 'xīn' ] ]
 console.log(pinyin("中心", {
   style: pinyin.STYLE_INITIALS, // 设置拼音风格
