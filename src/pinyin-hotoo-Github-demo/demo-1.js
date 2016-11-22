10fafcf56407941b338dfd8acf70ccd9fe121391
Engin <denganjun1234@qq.com>
2016-11-22 21:59:16 +0800

    add demo

--- src/pinyin-hotoo-Github-demo/demo-1.js
+++ src/pinyin-hotoo-Github-demo/demo-1.js
@@ -12,3 +12,31 @@ console.log(pinyin("中心", {
   style: pinyin.STYLE_INITIALS, // 设置拼音风格
   heteronym: true
 }));                            // [ [ 'zh' ], [ 'x' ] ]
+
+
+
+console.log(pinyin("中心银行，我看行。中国移动，还可以。", {
+  style: pinyin.STYLE_NORMAL, // 设置拼音风格
+  heteronym: false
+}));
+// 上面的句子，关闭了多音字模式,得出的结果，很多多音字存在问题。
+/*
+[ [ 'zhong' ],
+  [ 'xin' ],
+  [ 'yin' ],
+  [ 'hang' ],
+  [ '，' ],
+  [ 'wo' ],
+  [ 'kan' ],
+  [ 'hang' ],
+  [ '。' ],
+  [ 'zhong' ],
+  [ 'guo' ],
+  [ 'yi' ],
+  [ 'dong' ],
+  [ '，' ],
+  [ 'huan' ],
+  [ 'ke' ],
+  [ 'yi' ],
+  [ '。' ] ]
+*/
