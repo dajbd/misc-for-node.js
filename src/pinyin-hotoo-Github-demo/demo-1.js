7a5ad48f7b947843ce508affa0f9d5d948aefafe
Engin <denganjun1234@qq.com>
2016-11-22 21:53:19 +0800

    ignore segment function to continue error:  Segment need nodejieba, please run '$ npm install nodejieba'.  分词需要使用 nodejieba 模块，请运行 '$ npm install nodejieba' 并确保安装完成

--- src/pinyin-hotoo-Github-demo/demo-1.js
+++ src/pinyin-hotoo-Github-demo/demo-1.js
@@ -6,7 +6,7 @@ console.log(pinyin("中心", {
 }));                            // [ [ 'zhōng', 'zhòng' ], [ 'xīn' ] ]
 console.log(pinyin("中心", {
   heteronym: true,              // 启用多音字模式
-  segment: true                 // 启用分词，以解决多音字问题。
+  // segment: true                 // 启用分词，以解决多音字问题。
 }));                            // [ [ 'zhōng' ], [ 'xīn' ] ]
 console.log(pinyin("中心", {
   style: pinyin.STYLE_INITIALS, // 设置拼音风格
