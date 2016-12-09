1a40976560746d81213c358d1b810a38a4b2e4e6
Engin <denganjun1234@qq.com>
2016-12-09 18:13:37 +0800

    not work well

--- src/vue.js-demo-2/src/App.vue
+++ src/vue.js-demo-2/src/App.vue
@@ -1,25 +1,30 @@
 <template>
   <div id="app">
-    <img class="logo" src="./assets/logo.png">
+    <!-- <img class="logo" src="./assets/logo.png"> -->
+    <InfiniteScrollDemo></InfiniteScrollDemo>
+
     <hello></hello>
-  <back-to-top></back-to-top>
+    <back-to-top></back-to-top>
+  </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
 import BackToTop from './components/BackToTop'
+import InfiniteScrollDemo from './biz/infinite-scroll-demo'
 
 export default {
   components: {
     Hello,
-    BackToTop
+    BackToTop,
+    infinitescrolldemo: InfiniteScrollDemo
   }
 }
 </script>
 
 <style>
 #app{
-  min-height: 2500px;
+  /*min-height: 2500px;*/
   text-align: center;
 }
 </style>
---
+++ src/vue.js-demo-2/src/biz/infinite-scroll-demo.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="infinite-scroll-demo" :style="{ height: '300px' }">
+    <ul class="list"
+      v-infinite-scroll="loadMore"
+      infinite-scroll-disabled="loading"
+      infinite-scroll-distance="0">
+      <li v-for="item in list">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  // import InfiniteScroll from 'components/mint-ui/packages/infinite-scroll'
+  import { InfiniteScroll } from 'mint-ui'
+
+  Vue.use(InfiniteScroll)
+
+  export default {
+    // components: {
+    //   infinitescroll: InfiniteScroll
+    // },
+    data () {
+      return {
+        list: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      }
+    },
+    methods: {
+      loadMore () {
+        console.log('load')
+        this.loading = true
+        setTimeout(() => {
+          let last = this.list[this.list.length - 1]
+          for (let i = 1; i <= 10; i++) {
+            this.list.push(last + i)
+          }
+          this.loading = false
+        }, 2500)
+      }
+    },
+    ready () {
+      this.loadMore()
+    }
+  }
+  </script>
+
+<style lang="less">
+  .infinite-scroll-demo {
+    height: 300px;
+    // overflow-y: scroll;
+    overflow: scroll;
+
+    .list {
+      list-style: none;
+    }
+  }
+</style>
